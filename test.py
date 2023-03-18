8f8bf1af09c4b223f271418dc05b8f420227cd39
0xrsfagundes <email@youremail.com>
2023-03-18 18:55:12 +0000

    mar 18 2023

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
-import datetime
-today = datetime.date.today()
-year = today.year
+import plotly.express as px
+import pandas as pd
+
+# Define the data
+country = ['Brazil', 'US', 'IT']
+rates = [13, 45, 67]
+
+# Create a dataframe with the data
+data = {'Country': country, 'Rate': rates}
+df = pd.DataFrame(data)
+
+# Load the country codes dataset
+country_codes = px.data.gapminder().query("year == 2007")[["country", "iso_alpha"]]
+
+# Merge the data with the country codes dataset
+merged_df = pd.merge(df, country_codes, left_on="Country", right_on="iso_alpha")
+
+# Create a choropleth map
+fig = px.choropleth(merged_df, locations="Country", color="Rate", hover_name="Country",
+                    projection="natural earth")
+
+# Show the map
+fig.show()
