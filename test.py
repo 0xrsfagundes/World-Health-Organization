2cec669d27cb5c2a9802170314bcdfaf3683c7bd
0xrsfagundes <email@youremail.com>
2023-03-18 18:17:02 +0000

    mar 18 2023

--- test.py
+++ test.py
@@ -1,17 +1,3 @@
-
-import requests
-
-url = "https://ghoapi.azureedge.net/api/HIV_0000000001"
-
-response = requests.get(url)
-
-data = response.json()
-
-countries = []
-years = []
-rates = []
-
-for item in data['value']:
-    countries.append(item['SpatialDim'])
-    years.append(item['TimeDim'])
-    rates.append(item['NumericValue'])
+import datetime
+today = datetime.date.today()
+year = today.year
